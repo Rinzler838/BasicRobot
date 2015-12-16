03a71f9526c1df40feb578416b23bbff9232dd69
Rinzler838 <dvil4688@740s-pl-105.local>
2015-12-16 08:48:10 -0700

    Created basic Robot project

---
+++ src/bot/controller/DeigenBotRunner.java
@@ -0,0 +1,15 @@
+package bot.controller;
+/**
+ * @author dvil4688
+ * @version
+ */
+public class DeigenBotRunner
+{
+
+	public static void main(String[] args)
+	{
+		BotController myBotController = new BotController();
+		myBotController.start();
+	}
+
+}
